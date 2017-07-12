b8c9a29ef676622546a5b908e1e67619554a1444
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-12 10:15:52 -0600

    progress on displaying representatives

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,10 @@ class SearchController < ApplicationController
     connection = Faraday.new(url: "https://api.propublica.org/congress/v1/members/house/#{params[:state]}/current.json")
     connection.headers['X-API-Key'] = ENV['api_key']
     response = connection.get
-    raw_representatives = JSON.parse(response.body)
-    byebug
+    raw_representatives = JSON.parse(response.body)["results"]
+    representatives_backwards_sorted = raw_representatives.sort_by do |representative|
+      representative["seniority"].to_i
+    end
+    @representatives_sorted = representatives_backwards_sorted.reverse
   end
 end
