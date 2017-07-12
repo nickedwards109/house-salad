94a176eff283a81f65334bd627de583d47a9ea4a
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-12 10:43:39 -0600

    add a css selector, fix flash message to uppercase, and fix path spec

--- spec/features/user_gets_members_of_the_house_spec.rb
+++ spec/features/user_gets_members_of_the_house_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "User gets members of the house" do
     select "Colorado", from: "state"
     click_on "Locate Members of the House"
 
-    expect(page).to have_current_path(search_path(state: "CO"))
+    expect(page).to have_current_path(search_path, only_path: true)
     expect(page).to have_content("7 Results")
     expect(page).to have_selector(".house-member", count: 7)
     expect(page).to have_selector(".name", count: 7)
